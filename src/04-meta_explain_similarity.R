9edb54f75c789ee1009be43f1b1b69b294137b74
Pablo Hidalgo <pablohigar@gmail.com>
2023-12-04 17:35:11 +0100

    Minor changes

--- src/04-meta_explain_similarity.R
+++ src/04-meta_explain_similarity.R
@@ -16,8 +16,9 @@ cmd <- cmdArgs()
 
 ale_curves <- read_rds(cmd$ale_curves)
 models <- readRDS(cmd$models)
-
-
+# cmd <- list(same_features = FALSE)
+# models <- readRDS("empirical-work/synthetic-data-2/data/models.RDS")
+# ale_curves <- readRDS("empirical-work/synthetic-data-2/data/ale_by_task_var.RDS")
 # FRECHET DISTANCE --------------------------------------------------------
 
 # weight function for Frechet distance
@@ -31,7 +32,8 @@ for (task in names(models)){
 # compute Frechet distance
 frechet_results <- frechet_tasks_feature(ale_curves,
                                          importance = imp,
-                                         fweight = fweight
+                                         fweight = fweight,
+                                         same_features = cmd$same_features
                                          )
 
 # summary of results
@@ -41,6 +43,11 @@ similarity_task_summary <- frechet_task_summary(frechet_results)
 
 
 # DATA SAVING -------------------------------------------------------------
+
+saveRDS(
+  frechet_results, 
+  cmd$similarity
+)
 
 saveRDS(
   similarity_task_var_summary, 
