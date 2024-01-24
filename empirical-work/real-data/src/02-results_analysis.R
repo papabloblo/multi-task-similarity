62e29868697d65e73613b19323a0549d6722d95a
Pablo Hidalgo <pablohigar@gmail.com>
2024-01-24 11:07:43 +0100

    Real data analysis

--- empirical-work/real-data/src/02-results_analysis.R
+++ empirical-work/real-data/src/02-results_analysis.R
@@ -6,45 +6,56 @@
 # DEPENDENCIES ------------------------------------------------------------
 
 library(tidyverse)
-
+library(patchwork)
 
 
 # DATA IMPORT -------------------------------------------------------------
 
 similarity <- readRDS("empirical-work/real-data/data/similarity/similarity_task_summary.RDS")
+ale <- readRDS("empirical-work/real-data/data/ale_by_task_var.RDS")
+models <- readRDS("empirical-work/real-data/data/models.RDS")
+
+similarity_var <- readRDS("empirical-work/real-data/data/similarity/similarity_task_var_summary.RDS")
 
 
-# Similarity matrix plot
+# SIMILARITY MATRIX PLOT --------------------------------------------------
 
 p <- similarity %>% 
   mutate(
     task1 = as.numeric(task1),
     task2 = as.numeric(task2)
     ) %>% 
-  ggplot(aes(x = task1, y = task2, fill = similarity)) + 
+  ggplot(
+    aes(x = task1, 
+        y = task2, 
+        fill = similarity
+        )
+    ) + 
   geom_raster() + 
   theme_void() + 
-  theme(axis.text = element_text(size = 20),
-        legend.title = element_text(size = 20, face = "bold"),
-        legend.key.size = unit(1.5, "cm"),
-        legend.text = element_text(size = 20)
-        ) +
+  theme(
+    axis.text = element_text(size = 20),
+    legend.title = element_text(size = 20, face = "bold"),
+    legend.key.size = unit(1.5, "cm"),
+    legend.text = element_text(size = 20)
+    ) +
   labs(
     fill = "Multi-task\n similarity",
-    # x = "Patient 1",
-    # y = "Patient 2",
     ) +
-  scale_x_continuous(breaks = c(1, seq(5, 40, by = 5), 42),
-                     expand = expansion(0),
-                     position = "top") +
-  # scale_y_continuous(breaks = seq(5, 40, by = 5),
-  #                    expand = expansion(0)) +
-  scale_y_reverse(breaks = c(1, seq(5, 40, by = 5), 42),
-                  expand = expansion(0)) + 
-  scale_fill_viridis_c(option = "A", 
-                       # colors ordered from lightest to darkest
-                       direction = -1
-                       )
+  scale_x_continuous(
+    breaks = c(1, seq(5, 40, by = 5), 42),
+    expand = expansion(0),
+    position = "top"
+    ) +
+  scale_y_reverse(
+    breaks = c(1, seq(5, 40, by = 5), 42),
+    expand = expansion(0)
+    ) + 
+  scale_fill_viridis_c(
+    option = "A", 
+    # colors ordered from lightest to darkest
+    direction = -1
+    )
 
 
 ggsave(p, 
@@ -58,7 +69,7 @@ ggsave(p,
 #' the vast majority of the other patients.
 
 similarity %>% 
-  filter(task1 %in% c("15", "14")) %>% 
+  filter(task1 %in% c("15", "11")) %>% 
   View()
 
 
@@ -72,175 +83,145 @@ similarity %>%
 #' and with respect the five least similar patients
 #' 
 
-ale <- readRDS("empirical-work/real-data/data/ale_by_task_var.RDS")
-models <- readRDS("empirical-work/real-data/data/models.RDS")
-
-most_imp_var <- names(sort(models[["15"]]$importance, decreasing = TRUE)[1:3])
-
-sort(models[["15"]]$importance, decreasing = TRUE)[1:3]
-sort(models[["14"]]$importance, decreasing = TRUE)[1:3]
-
-ale %>% 
-  filter(
-    task %in% as.character(c(15, 9, 19, 29, 26, 24)),
-    feature %in% most_imp_var) %>% 
-  mutate(
-    highlight = ifelse(task == "15", "1", "0")
-  ) %>% 
-  ggplot(aes(x = x, y = ale)) + 
-  geom_line(aes(group = task, color = highlight)) + 
-  facet_wrap(. ~ feature, ncol = 3, scales = "free_x")
-
-
-
-
-ale %>% 
-  filter(
-    task %in% as.character(c(15, 39, 18, 22, 36, 28)),
-    feature %in% most_imp_var) %>% 
-  mutate(
-    highlight = ifelse(task == "15", "1", "0")
-  ) %>% 
-  ggplot(aes(x = x, y = ale)) + 
-  geom_line(aes(group = task, color = highlight)) + 
-  facet_wrap(. ~ feature, ncol = 3, scales = "free_x")
-
-
-ale %>% 
-  filter(
-    task %in% as.character(c(14, 37, 13, 1, 2, 25)),
-    feature %in% most_imp_var) %>% 
-  mutate(
-    highlight = ifelse(task == "14", "1", "0")
-  ) %>% 
-  ggplot(aes(x = x, y = ale)) + 
-  geom_line(aes(group = task, color = task)) + 
-  facet_wrap(. ~ feature, ncol = 3, scales = "free_x")
-
-ale %>% 
-  filter(
-    task %in% as.character(c(14, 29, 15, 35, 31, 24)),
-    feature %in% most_imp_var) %>% 
-  mutate(
-    highlight = ifelse(task == "14", "1", "0")
-  ) %>% 
-  ggplot(aes(x = x, y = ale)) + 
-  geom_line(aes(group = task, color = task)) + 
-  facet_wrap(. ~ feature, ncol = 3, scales = "free_x")
-
-
 
 # Most similar using the most important feature
 
+
 plot_ale_similar <- function(task_orig, 
-                            ale, 
-                            models,
-                            similarity, 
-                            n_tasks = 5,
-                            n_feat = 1,
-                            most = TRUE){
-  most_imp_var <- names(
+                             ale, 
+                             models,
+                             similarity, 
+                             n_tasks = 5,
+                             n_feat = 3,
+                             most = TRUE, 
+                             ribbon = 1,
+                             limits_y = NULL,
+                             n.breaks_x = 4){
+  most_imp_var <-
     sort(
-      models[[task]]$importance, 
+      models[[task_orig]]$importance, 
       decreasing = TRUE
-      )[1:n_feat]
-    )
-  
+    )[1:n_feat]
   
   aux <- similarity[similarity$task1 == task_orig,]
   most_sim_tasks <- aux$task2[order(aux$similarity, decreasing = !most)[1:n_tasks]]
   
-  ale %>% 
+  df <- ale %>% 
     filter(
       task %in% c(task_orig, most_sim_tasks),
-      feature %in% most_imp_var
-      ) %>% 
+      feature %in% names(most_imp_var)) %>% 
+    group_by(task, feature) %>% 
     mutate(
       highlight = ifelse(task == task_orig, "1", "0"),
-      feature = factor(feature, levels = most_imp_var, ordered = TRUE)
-    ) %>% 
-    ggplot(aes(x = x, y = ale)) + 
-    geom_line(aes(group = task, color = highlight)) + 
-    facet_wrap(. ~ feature, ncol = n_tasks, scales = "free_x") +
+      task = factor(task, levels = c(most_sim_tasks, task_orig), ordered = TRUE),
+      # task = paste("Task", task),
+      n2 = n/max(n),
+      ymax = ale +  ribbon*n2,
+      ymin = ale -  ribbon*n2,
+      
+      feature = paste0(feature, " (Importance: ",round(most_imp_var[feature], 2)*100, "%)"),
+      feature = factor(feature, 
+                       levels = paste0(names(most_imp_var), " (Importance: ",round(most_imp_var, 2)*100, "%)"),
+                       ordered = TRUE), 
+      ) %>% 
+    ungroup()
+  
+  p <- df %>% 
+    filter(task != task_orig) %>% 
+    ggplot( aes(y = ale, x = x, group = task, 
+               color = highlight, fill = highlight,
+               ymax = ymax, ymin = ymin
+               )
+            ) +
+    geom_ribbon(alpha = 0.7) +
+    geom_line() +  
+    
+    geom_ribbon(data = filter(df, task == task_orig),
+                alpha = 0.7) +
+    geom_line(data = filter(df, task == task_orig)) +  
     labs(y = "",
          x = ""
     ) +
     theme_minimal() +
+    scale_y_continuous(limits = limits_y, n.breaks = n.breaks) + 
+    scale_x_continuous(n.breaks = n.breaks_x) +
+    scale_color_manual(values = c("1" = "gray25", "0" = "gray80")) + 
+    scale_fill_manual(values = c("1" = "gray25", "0" = "gray80")) + 
     theme(
       strip.text = element_text(size = 6),
-      panel.spacing.x = unit(0, "pt"),
       strip.background = element_rect(fill = "gray90", color = NA),
-      plot.margin = margin(0, 0, 0, 0, "cm")
-    )
+      legend.position = "none"
+    ) +
+    facet_wrap(. ~ feature, ncol = n_tasks, scales = "free_x")
   
-  
+  if (most){
+    p + labs(title = paste("Most similar tasks to task", task_orig))
+  } else {
+    p + labs(title = paste("Least similar tasks to task", task_orig))
+  }
 }
 
 
-plot_ale_similar("15", 
+limits_y <- c(-6, 5)
+n.breaks_x <- 4
+
+p1 <- plot_ale_similar("14", 
+                       ale, 
+                       models,
+                       similarity,
+                       n_tasks = 5,
+                       n_feat = 2,
+                       most = TRUE,
+                       ribbon = .4,
+                       limits_y = limits_y,
+                       n.breaks_x = n.breaks_x
+) 
+
+p2 <- plot_ale_similar("14", 
+                       ale, 
+                       models,
+                       similarity,
+                       n_tasks = 5,
+                       n_feat = 2,
+                       most = FALSE,
+                       ribbon = .4,
+                       limits_y = limits_y,
+                       n.breaks_x = n.breaks_x
+) 
+p3 <- plot_ale_similar("15", 
                  ale, 
                  models,
                  similarity,
-                 n_tasks = 3,
-                 n_feat = 1,
-                 most = TRUE)
+                 n_tasks = 5,
+                 n_feat = 2,
+                 most = TRUE,
+                 ribbon = .4,
+                 limits_y = limits_y,
+                 n.breaks_x = n.breaks_x
+                 ) 
 
-
-plot_ale_similar("14", 
+p4 <- plot_ale_similar("15", 
                  ale, 
                  models,
                  similarity,
-                 n_tasks = 3,
-                 n_feat = 1,
-                 most = TRUE)
+                 n_tasks = 5,
+                 n_feat = 2,
+                 most = FALSE,
+                 ribbon = .4,
+                 limits_y = limits_y,
+                 n.breaks_x = n.breaks_x
+                 ) 
 
 
-plot_ale_similar("14", 
-                 ale, 
-                 models,
-                 similarity,
-                 n_tasks = 3,
-                 n_feat = 1,
-                 most = FALSE)
 
 
-plot_ale_similar("13", 
-                 ale, 
-                 models,
-                 similarity,
-                 n_tasks = 10,
-                 n_feat = 1,
-                 most = TRUE)
+p <- p1 + p2 + p3 + p4 
 
-
-plot_ale_similar("13", 
-                 ale, 
-                 models,
-                 similarity,
-                 n_tasks = 10,
-                 n_feat = 1,
-                 most = FALSE)
-
-
-ale %>% 
-  filter(
-    task %in% as.character(c(15, 9, 19, 29, 26, 24)),
-    feature == "test_time") %>% 
-  mutate(
-    highlight = ifelse(task == "15", "1", "0")
-  ) %>% 
-  ggplot(aes(x = x, y = ale)) + 
-  geom_line(aes(group = task, color = highlight))
-
-
-ale %>% 
-  filter(
-    task %in% as.character(c(14, 37, 13, 1, 2, 25)),
-    feature == "test_time") %>% 
-  mutate(
-    highlight = ifelse(task == "14", "1", "0")
-  ) %>% 
-  ggplot(aes(x = x, y = ale)) + 
-  geom_line(aes(group = task, color = highlight)) + 
-  facet_wrap(. ~ feature, ncol = 3, scales = "free_x")
-
+ggsave(
+  filename = "empirical-work/real-data/plots/most-least-sim-tasks-14-15.png", 
+  plot = p,
+  width = 25,
+  height = 20,
+  dpi = "print",
+  units = "cm"
+  )
